81d1fb772228b3bd62e5e4a634d08ee3e03444aa
Zainab Saad <zsaad.bese21seecs@seecs.edu.pk>
2023-05-02 00:06:14 +0500

    Add code files for loading csv to age

---
+++ test_load_csv_to_age.py
@@ -0,0 +1,13 @@
+import age, load_csv_to_age as ld
+
+
+GRAPH_NAME = "csv_load_age"
+DSN = "host=127.0.0.1 port=5432 dbname=agedb user=zainab password=Zainab2002"
+# connect to postgreSQL database using AGE extension
+age_obj = age.Age()
+ag = age_obj.connect(graph=GRAPH_NAME, dsn= DSN)
+
+# test the function 
+ld.load_labels_from_file(ag, './age_load/data/countries.csv', 'Country')
+ld.load_labels_from_file(ag, './age_load/data/cities.csv', 'City')
+ld.load_edges_from_file(ag, './age_load/data/edges.csv', 'BELONGS_TO')

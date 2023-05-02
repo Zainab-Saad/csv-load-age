32cd2f18060d93da1469c6b253db24799aa4a9a9
Zainab Saad <zsaad.bese21seecs@seecs.edu.pk>
2023-05-02 09:42:30 +0500

    Update test file

--- test_load_age_to_csv.py
+++ test_load_age_to_csv.py
@@ -2,7 +2,7 @@ import age, load_age_to_csv as ld
 
 
 GRAPH_NAME = "csv_load_age"
-DSN = "host=127.0.0.1 port=5432 dbname=agedb user=zainab password=Zainab2002"
+DSN = "host=127.0.0.1 port=5432 dbname=postgres user=postgres password=12345"
 # connect to postgreSQL database using AGE extension
 age_obj = age.Age()
 ag = age_obj.connect(graph=GRAPH_NAME, dsn= DSN)
